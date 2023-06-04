31ccde9d5a0fcfa389278cb656887fa424d472ba
faisal-saddique <faisalsaddique7964@gmail.com>
2023-06-04 15:41:36 +0500

    opening PDFs

--- app.py
+++ app.py
@@ -1,96 +1,148 @@
-import streamlit as st
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from langchain.llms import OpenAI
+# import streamlit as st
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+# from langchain.llms import OpenAI
+# import os
+# from dotenv import load_dotenv
+# from langchain.chat_models import ChatOpenAI
+# from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+# from open_pdf_from_page_and_zoom import display_pdf_with_zoom
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+# api_key = os.getenv("OPENAI_API_KEY")
+# os.environ['OPENAI_API_KEY'] = api_key
+
+# embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+# model_name = "text-davinci-003"  # the model used for text generation
+# chain_type = "refine"  # Results returned using 'refine' are detailed as compared to 'map_reduce', but you could try to play around with these things.
+
+# new_db = FAISS.load_local("ash_local_FAISS_index", embeddings)
+
+
+# def create_response(matched_chunks, question):
+    
+#     llm = ChatOpenAI(
+#         model_name="gpt-3.5-turbo",
+#         temperature=.1
+#     )
+
+#     # You can assign the system a role here. You play around this prompt to see how the response changes.
+
+#     system_template = """A ___ bot/assistant/etc that answers questions about Ash's work, design, process, ideas; as well as Ash's reading and research, and specifies when it uses Ash's reading / secondary research."""
+#     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+#     # Please update this prompt below to add instructions as to which symbols mean which chunk is that related to. 
+
+#     human_template = """This text provides context as to whether the information is Ash's own notes/design,
+#     or his notes/research on the thoughts and work of others, or his Context Report/Dissertation. Notes/research
+#     and Context Report/Dissertation likely include some of his Design, which will be indicated:
+    
+#     ```{matched_chunks}```
+    
+#     Please now answer this query based on the text I shared with you:
+    
+#     QUESTION:
+#     {question}"""
+
+#     human_message_prompt = HumanMessagePromptTemplate.from_template(
+#         human_template)
+
+#     chat_prompt = ChatPromptTemplate.from_messages(
+#         [system_message_prompt, human_message_prompt])
+
+#     response = llm(chat_prompt.format_prompt(
+#         matched_chunks=matched_chunks,question=question).to_messages()).content
+
+#     return response
+
+
+# st.title("PDF Search and QA App")
+
+# query = st.text_input("Please enter your query:")
+
+# # if st.button("press me"):
+# #     display_pdf_with_zoom('test.pdf',13,200)
+
+# if query:
+#     docs = new_db.similarity_search(query, k=3)  # k = 3; Return top 3 results
+
+#     relevant_content_and_pages = [{"page_number":doc.metadata["page_number"],"content":doc.page_content} for doc in docs]
+#     # relevant_pages = [doc.metadata['page_number'] for doc in docs]
+
+#     st.write(f"Relevant Page numbers found are: {', '.join(map(str, [item['page_number'] for item in relevant_content_and_pages]))}")
+
+#     for page in [item['page_number'] for item in relevant_content_and_pages]:
+#         display_pdf_with_zoom(pdf_filename="test.pdf",page_number=page,zoom_level=300)
+#         st.write("opened page in PDF")
+
+#     with st.expander("View relevant chunks"):
+#         # Iterate over the relevant_content_and_pages list
+#         for item in relevant_content_and_pages:
+#             page_number = item["page_number"]
+#             content = item["content"]
+
+#             st.write(f"Page number # {page_number}")
+#             st.write(content)
+
+    
+#     # chain = load_qa_with_sources_chain(OpenAI(temperature=0, model_name=model_name, max_tokens=-1), chain_type=chain_type)  # -1 sets the limit to the best what is available with the current model
+#     # result = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
+
+#     result = create_response(matched_chunks="\n".join([obj['content'] for obj in relevant_content_and_pages]),question=query)
+    
+#     st.write(f"Response: {result}")
+
 import os
-from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
-from open_pdf_from_page_and_zoom import display_pdf_with_zoom
+import subprocess
+import streamlit as st
 
-# Load environment variables from .env file
-load_dotenv()
+def display_pdf_with_zoom(pdf_filename, page_number, zoom_level):
+    def get_chrome_path():
+        # Platform-specific commands to find Chrome executable
+        if os.name == 'nt':  # Windows
+            key_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe"
+            try:
+                import winreg
+                key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_READ)
+                value, _ = winreg.QueryValueEx(key, None)
+                return value
+            except ImportError:
+                pass
 
-api_key = os.getenv("OPENAI_API_KEY")
-os.environ['OPENAI_API_KEY'] = api_key
+        elif os.name == 'posix':  # macOS and Linux
+            try:
+                chrome_path = subprocess.check_output(['which', 'google-chrome'], text=True).strip()
+                return chrome_path
+            except subprocess.CalledProcessError:
+                pass
 
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-model_name = "text-davinci-003"  # the model used for text generation
-chain_type = "refine"  # Results returned using 'refine' are detailed as compared to 'map_reduce', but you could try to play around with these things.
+        return None
 
-new_db = FAISS.load_local("ash_local_FAISS_index", embeddings)
+    # Get the installation path of Google Chrome
+    chrome_path = get_chrome_path()
 
+    if chrome_path:
+        # Get the current directory path
+        current_directory = os.getcwd()
 
-def create_response(matched_chunks, question):
-    
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=.1
-    )
+        # Construct the complete path to the PDF file
+        pdf_path = os.path.join(current_directory, pdf_filename)
 
-    # You can assign the system a role here. You play around this prompt to see how the response changes.
+        # Construct the URL with the zoom level and page number as raw literals
+        url = rf"file://{pdf_path}#zoom={zoom_level}&page={page_number}"
 
-    system_template = """A ___ bot/assistant/etc that answers questions about Ash's work, design, process, ideas; as well as Ash's reading and research, and specifies when it uses Ash's reading / secondary research."""
-    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+        st.write(url)
+        # Instead of opening the URL in Chrome, display it as a link in Streamlit
+        st.markdown(f"[Open PDF with Zoom Level {zoom_level} and Page {page_number}]({url})")
+    else:
+        st.write("Chrome not found.")
 
-    # Please update this prompt below to add instructions as to which symbols mean which chunk is that related to. 
+# Usage example:
+pdf_filename = 'test.pdf'
+page_number = 23
+zoom_level = 50
 
-    human_template = """This text provides context as to whether the information is Ash's own notes/design,
-    or his notes/research on the thoughts and work of others, or his Context Report/Dissertation. Notes/research
-    and Context Report/Dissertation likely include some of his Design, which will be indicated:
-    
-    ```{matched_chunks}```
-    
-    Please now answer this query based on the text I shared with you:
-    
-    QUESTION:
-    {question}"""
-
-    human_message_prompt = HumanMessagePromptTemplate.from_template(
-        human_template)
-
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_message_prompt, human_message_prompt])
-
-    response = llm(chat_prompt.format_prompt(
-        matched_chunks=matched_chunks,question=question).to_messages()).content
-
-    return response
-
-
-st.title("PDF Search and QA App")
-
-query = st.text_input("Please enter your query:")
-
-# if st.button("press me"):
-#     display_pdf_with_zoom('test.pdf',13,200)
-
-if query:
-    docs = new_db.similarity_search(query, k=3)  # k = 3; Return top 3 results
-
-    relevant_content_and_pages = [{"page_number":doc.metadata["page_number"],"content":doc.page_content} for doc in docs]
-    # relevant_pages = [doc.metadata['page_number'] for doc in docs]
-
-    st.write(f"Relevant Page numbers found are: {', '.join(map(str, [item['page_number'] for item in relevant_content_and_pages]))}")
-
-    for page in [item['page_number'] for item in relevant_content_and_pages]:
-        display_pdf_with_zoom(pdf_filename="test.pdf",page_number=page,zoom_level=300)
-        st.write("opened page in PDF")
-
-    with st.expander("View relevant chunks"):
-        # Iterate over the relevant_content_and_pages list
-        for item in relevant_content_and_pages:
-            page_number = item["page_number"]
-            content = item["content"]
-
-            st.write(f"Page number # {page_number}")
-            st.write(content)
-
-    
-    # chain = load_qa_with_sources_chain(OpenAI(temperature=0, model_name=model_name, max_tokens=-1), chain_type=chain_type)  # -1 sets the limit to the best what is available with the current model
-    # result = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
-
-    result = create_response(matched_chunks="\n".join([obj['content'] for obj in relevant_content_and_pages]),question=query)
-    
-    st.write(f"Response: {result}")
+display_pdf_with_zoom(pdf_filename, page_number, zoom_level)
